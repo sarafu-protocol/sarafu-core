ad916681439f16ac4af9dd4153108fcf62dac0e9
Emmanuel-Odero <emmanuelodero808@gmail.com>
2026-02-20 13:50:02 +0300

    test: implement test utilities

---
+++ tests/test_utils.cpp
@@ -0,0 +1,11 @@
+// Test utilities for Sarafu blockchain tests
+#include "test_utils.h"
+
+namespace sarafu {
+namespace test {
+
+// Placeholder for test utilities
+// Will be populated as we implement components
+
+} // namespace test
+} // namespace sarafu
